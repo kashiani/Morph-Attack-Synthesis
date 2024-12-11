3f38b8c617e9dbbbb767bb92b095e7c5c0729aa9
kashiani <hkashia@clemson.edu>
2024-12-11 00:11:25 -0500

    Instantiate the landmark detector globally

--- src/inversion/image_inverter.py
+++ src/inversion/image_inverter.py
@@ -10,6 +10,9 @@ from src.morphing.latent_morpher import latent_morpher
 from src.utils.file_utils import generate_file_path, make_dir
 from ffhq_dataset.landmarks_detector import LandmarksDetector
 
+# Instantiate the landmark detector globally
+landmark_detector = LandmarksDetector("./inversion/weights/shape_predictor_68_face_landmarks.dat")
+
 def i2s(img1: str, img2: str, network_pkl: str, num_steps: int, output_dir: str):
     """
     Projects images to StyleGAN latent space using I2S method without any warping and morphs the latents.
