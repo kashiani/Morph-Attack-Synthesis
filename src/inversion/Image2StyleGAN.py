963f44d69af416535ac1f13fb0dac509a934672e
kashiani <hkashia@clemson.edu>
2024-12-13 11:03:49 -0500

    Morphing module initialization. Image2StyleGAN

--- src/inversion/Image2StyleGAN.py
+++ src/inversion/Image2StyleGAN.py
@@ -36,3 +36,32 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # Instantiate the landmark detector globally
 landmark_detector = LandmarksDetector("./inversion/weights/shape_predictor_68_face_landmarks.dat")
+
+def project(
+    G,
+    target: torch.Tensor, # [C,H,W] and dynamic range [0,255], W & H must match G output resolution
+    *,
+    num_steps                  = 1000,
+    w_avg_samples              = 10000,
+    initial_learning_rate      = 0.1,
+    initial_noise_factor       = 0.05,
+    lr_rampdown_length         = 0.25,
+    lr_rampup_length           = 0.05,
+    noise_ramp_length          = 0.75,
+    euclidean_dist_weight      = 0.0,
+    pixel_weight               = 0.01,
+    mtcnn_weight               = 1,
+    regularize_mag_weight      = 0.1,
+    dist_weight                = 1,
+    regularize_noise_weight    = 1e5,
+    verbose                    = False,
+    vgg_weights                = 'weights/vgg16.pt',
+    output                     = "",
+    device: torch.device
+):
+    assert target.shape == (G.img_channels, G.img_resolution, G.img_resolution)
+
+    def logprint(*args):
+        if verbose:
+            print(*args)
+
