b0911aad73d96ff2a8bb08838a8f4b045c683c86
kashiani <hkashia@clemson.edu>
2024-12-14 15:51:42 -0500

    Morphing module initialization. Landmark-Based Inversion

---
+++ src/inversion/landmark_inverter.py
@@ -0,0 +1,6 @@
+"""
+Landmark-Based Inversion Methods
+"""
+import os
+from src.inversion.Image2StyleGAN_convex_hull import projection as landmark_projection
+from src.inversion.Image2StyleGAN import align_image
