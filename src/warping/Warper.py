099d016324bbd413fcfae689ebeb21f28e13a167
kashiani <hkashia@clemson.edu>
2024-12-19 18:45:32 -0500

    list_imgpaths

--- src/warping/Warper.py
+++ src/warping/Warper.py
@@ -47,3 +47,4 @@ def list_imgpaths(images_folder=None, src_image=None, dest_image=None):
          fname.lower().endswith('.png') or
          fname.lower().endswith('.jpeg')):
         yield os.path.join(images_folder, fname)
+
