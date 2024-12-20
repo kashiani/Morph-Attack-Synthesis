42f9d975b2c30d0ee3f9330dd5f6d3bec6696c98
kashiani <hkashia@clemson.edu>
2024-12-20 14:00:25 -0500

    roi_coordinates

--- src/warping/warp_support/aligner.py
+++ src/warping/warp_support/aligner.py
@@ -14,3 +14,25 @@ def positive_cap(num):
     return 0, abs(num)
   else:
     return num, 0
+
+
+def roi_coordinates(rect, size, scale):
+  """ Align the rectangle into the center and return the top-left coordinates
+  within the new size. If rect is smaller, we add borders.
+
+  :param rect: (x, y, w, h) bounding rectangle of the face
+  :param size: (width, height) are the desired dimensions
+  :param scale: scaling factor of the rectangle to be resized
+  :returns: 4 numbers. Top-left coordinates of the aligned ROI.
+    (x, y, border_x, border_y). All values are > 0.
+  """
+  rectx, recty, rectw, recth = rect
+  new_height, new_width = size
+  mid_x = int((rectx + rectw/2) * scale)
+  mid_y = int((recty + recth/2) * scale)
+  roi_x = mid_x - int(new_width/2)
+  roi_y = mid_y - int(new_height/2)
+
+  roi_x, border_x = positive_cap(roi_x)
+  roi_y, border_y = positive_cap(roi_y)
+  return roi_x, roi_y, border_x, border_y
