4ba1b189188fa1f9c97ed8632e8d50fe69141fe7
kashiani <hkashia@clemson.edu>
2024-12-20 14:00:14 -0500

    positive_cap

---
+++ src/warping/warp_support/aligner.py
@@ -0,0 +1,16 @@
+"""
+Align face and image sizes
+"""
+import cv2
+import numpy as np
+
+def positive_cap(num):
+  """ Cap a number to ensure positivity
+
+  :param num: positive or negative number
+  :returns: (overflow, capped_number)
+  """
+  if num < 0:
+    return 0, abs(num)
+  else:
+    return num, 0
