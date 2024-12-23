38416f1221a666c0114fa7ca12a6f3ca3d46af90
kashiani <hkashia@clemson.edu>
2024-12-23 16:51:49 -0500

    check_do_save

--- src/warping/warp_support/plotter.py
+++ src/warping/warp_support/plotter.py
@@ -32,3 +32,18 @@ def check_do_plot(func):
         if self.do_plot:
             func(self, *args, **kwargs)
     return inner
+
+def check_do_save(func):
+    """
+    Decorator to execute a save function only if `do_save` is enabled.
+
+    :param func: function
+        The function to conditionally execute.
+
+    :returns: function
+        Wrapped function that checks `do_save` before execution.
+    """
+    def inner(self, *args, **kwargs):
+        if self.do_save:
+            func(self, *args, **kwargs)
+    return inner
