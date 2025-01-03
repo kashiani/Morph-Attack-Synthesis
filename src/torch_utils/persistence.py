cad763ca2bfab71fa095675996aec590e6bd14ff
kashiani <hkashia@clemson.edu>
2025-01-03 11:35:42 -0500

    Internal versioning and tracking variables

--- src/torch_utils/persistence.py
+++ src/torch_utils/persistence.py
@@ -12,3 +12,19 @@ The pickled code is automatically imported into a separate Python module
 during unpickling. This way, any previously exported pickles will remain
 usable even if the original code is no longer available, or if the current
 version of the code is not consistent with what was originally pickled."""
+
+import sys
+import pickle
+import io
+import inspect
+import copy
+import uuid
+import types
+import dnnlib
+
+# Internal versioning and tracking variables
+_version = 6  # Internal version number
+_decorators = set()  # Set of decorator classes
+_import_hooks = []  # List of import hook functions
+_module_to_src_dict = dict()  # Mapping of modules to source code
+_src_to_module_dict = dict()  # Mapping of source code to modules
